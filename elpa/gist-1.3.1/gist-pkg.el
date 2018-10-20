2938dd133bb296c9ae185de2b5e0599535b506b4
linhx13 <mylhx288@gmail.com>
2018-10-20 20:41:50 +0800

    Add elpa

---
+++ elpa/gist-1.3.1/gist-pkg.el
@@ -0,0 +1,1 @@
+(define-package "gist" "1.3.1" "Emacs integration for gist.github.com" '((emacs "24.1") (gh "0.9.2")) :url "https://github.com/defunkt/gist.el" :keywords '("tools"))
---
+++ elpa/protobuf-mode-3.5.0/protobuf-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "protobuf-mode" "3.5.0" "major mode for editing protocol buffers." 'nil :commit "2761122b810fe8861004ae785cc3ab39f384d342" :keywords '("google" "protobuf" "languages"))
